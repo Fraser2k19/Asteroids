1e9ee07af553c5b124b1a92f96786b6948afc1fb
Fraser2k19 <fraser2k19@gmail.com>
2025-06-05 10:50:13 +0100

    Update player.py

--- player.py
+++ player.py
@@ -10,7 +10,7 @@ class Player(CircleShape):
         self.shoot_timer = 0
 
     
-    # in the player class
+   
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
